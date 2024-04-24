64e4357eedc63291b6f3948a3479124e28e9d2f9
Søren <98187310+bergsoren@users.noreply.github.com>
2024-04-24 14:37:17 +0200

    created first step and settings file for everything that needs to be changed by the user, added docstring

---
+++ STEP1_create_biomes.py
@@ -0,0 +1,13 @@
+"""
+Welcome to the simple things: this code uses PyTorch (https://github.com/pytorch/pytorch) and
+creates monthly generated basin wide fCO2 maps derrived from the SOM. The
+input file specifies the year and month and the lat-lon area. 
+
+Original matlab code by:
+Peter Landschutzer 17.01.2012
+University of East Anglia, Norwich
+
+Python version by:
+Søren Jakob Berger 24.04.2024
+Max-Planck-Institut für Meteorologie, Hamburg
+"""
---
+++ settings.py
@@ -0,0 +1,5 @@
+"""
+This file includes all settings and configurables
+-> Everything that needs to be changed for the code to work will be found in this central location
+"""
+
