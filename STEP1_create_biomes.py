26530d9e1927e25fd36944e8a4d44a7486623ac6
Søren <98187310+bergsoren@users.noreply.github.com>
2024-04-24 16:52:24 +0200

    implemented Input_Training_and_Labelling_new_GUI_v2021.m

--- STEP1_create_biomes.py
+++ STEP1_create_biomes.py
@@ -1,7 +1,8 @@
 """
-Welcome to the simple things: this code uses PyTorch (https://github.com/pytorch/pytorch) and
-creates monthly generated basin wide fCO2 maps derrived from the SOM. The
-input file specifies the year and month and the lat-lon area. 
+Welcome to the simple things: this code uses PyTorch
+(https://github.com/pytorch/pytorch) and creates monthly generated basin wide
+fCO2 maps derrived from the SOM. The input file specifies the year and month
+and the lat-lon area. 
 
 Original matlab code by:
 Peter Landschutzer 17.01.2012
@@ -11,3 +12,67 @@ Python version by:
 Søren Jakob Berger 24.04.2024
 Max-Planck-Institut für Meteorologie, Hamburg
 """
+
+#========IMPORTS====
+import numpy as np
+
+import settings
+import debug
+
+
+#========Input_Training_and_Labelling_new_GUI_v2021.m====
+year_min = settings.INPUT_TIME['year_min']
+year_max = settings.INPUT_TIME['year_max']
+year2find = settings.INPUT_TIME['year2find']
+month2plot = settings.INPUT_TIME['month2plot']
+
+year_output = np.arange(year_min, year_max+1)
+timevec = np.arange(1980, year_max+1+1/12, 1/12)
+
+loncropmin = settings.INPUT_GEOBORDERS['lonmin']
+loncropmax = settings.INPUT_GEOBORDERS['lonmax']
+latcropmin = settings.INPUT_GEOBORDERS['latmin']
+latcropmax = settings.INPUT_GEOBORDERS['latmax']
+invareamin = settings.INPUT_GEOBORDERS['invareamin']
+invareamax = settings.INPUT_GEOBORDERS['invareamax']
+lonmin = settings.INPUT_GEOBORDERS['lonplotmin']
+lonmax = settings.INPUT_GEOBORDERS['lonplotmax']
+latmin = settings.INPUT_GEOBORDERS['latplotmin']
+latmax = settings.INPUT_GEOBORDERS['latplotmax']
+version = settings.INPUT_GEOBORDERS['version']
+
+FFN_go = settings.INPUT_METHOD['FFN']
+SOM_go = settings.INPUT_METHOD['SOM']
+CL_go = settings.INPUT_METHOD['Clustering']
+MLR_go = settings.INPUT_METHOD['MLR']
+BIOME_go = settings.INPUT_METHOD['BIOME']
+SSOM_go = settings.INPUT_METHOD['SSOM']
+
+if(FFN_go == True or BIOME_go == True):
+    net2take = settings.INPUT_METHOD_FFN_OR_BIOME['net2take']
+    netlayer = settings.INPUT_METHOD_FFN_OR_BIOME['netlayer']
+    layer2take = settings.INPUT_METHOD_FFN_OR_BIOME['layer2take']
+    nnnumber = settings.INPUT_METHOD_FFN_OR_BIOME['nnnumber']
+    load_trained_net = settings.INPUT_METHOD_FFN_OR_BIOME['load_trained_net']
+
+if(SOM_go == True or BIOME_go == True):
+    SOMnr = settings.INPUT_METHOD_SOM_OR_BIOME['SOMnr']
+    maplength = settings.INPUT_METHOD_SOM_OR_BIOME['maplength']
+    maphight = settings.INPUT_METHOD_SOM_OR_BIOME['maphight']
+    epochnr = settings.INPUT_METHOD_SOM_OR_BIOME['epochnr']
+    load_trained_SOM = settings.INPUT_METHOD_SOM_OR_BIOME['load_trained_SOM']
+    hc_clusters = settings.INPUT_METHOD_SOM_OR_BIOME['hc_clusters']
+
+if(CL_go == True):
+    clusternr = settings.INPUT_METHOD_CL['clusternr']
+    nb_cluster = settings.INPUT_METHOD_CL['nb_cluster']
+
+if(MLR_go == True):
+    MLRnr = settings.INPUT_METHOD_MLR['MLRnr']
+
+if(SSOM_go == True):
+    SSOMnr = settings.INPUT_METHOD_SSOM['SSOMnr']
+    nnnumber = settings.INPUT_METHOD_SSOM['nnnumber']
+
+
+#========1) load cluster data ====
--- settings.py
+++ settings.py
@@ -7,9 +7,37 @@ this central location.
 #========Input_Training_and_Labelling_new_GUI_v2021.m====
 INPUT_TIME = {'year_min': 1982, 'year_max': 2020, 'year2find': 2005,
               'month2plot': 1}
+"""Configure here what was before in the input dialog 'please enter time'."""
 
 INPUT_GEOBORDERS = {'latmin': -90, 'latmax': 90, 'lonmin': -180,
-                    'lonmax': 180,'invareamin': 1,'invareamax': 11,
+                    'lonmax': 180, 'invareamin': 1, 'invareamax': 11,
                     'latplotmin': -90, 'latplotmax': 90, 'lonplotmin': -180,
                     'lonplotmax': 180, 'version': 2};
+"""Configure here what was before in the input dialog 'please enter geographic
+boarders'."""
 
+INPUT_METHOD = {'FFN': False, 'SOM': False, 'Clustering': False, 'MLR': False,
+                'BIOME': True, 'SSOM': False}
+"""Configure here what was before in the input dialog 'please enter method'."""
+
+INPUT_METHOD_FFN_OR_BIOME = {
+    'net2take': 'GCBv2020_GCBv2020a-combined_smoothed', 'netlayer': '2layer',
+    'layer2take': '2layer_plus_coast_v1p5', 'nnnumber': 50,
+    'load_trained_net': 'yes or no'}
+"""If INPUT_METHOD['FFN'] == True or INPUT_METHOD['BIOME'] == True further
+enter method."""
+
+INPUT_METHOD_SOM_OR_BIOME = {'SOMnr': 'SOM', 'maplength': 2, 'maphight': 2,
+                             'epochnr': 5, 'load_trained_SOM': 'yes or no',
+                             'hc_clusters': 10}
+"""If INPUT_METHOD['SOM'] == True or INPUT_METHOD['BIOME'] == True further
+enter method."""
+
+INPUT_METHOD_CL = {'clusternr': 'cl100', 'nb_cluster': 100}
+"""If INPUT_METHOD['CL'] == True further enter method."""
+
+INPUT_METHOD_MLR = {'MLRnr': 'MLR100'}
+"""If INPUT_METHOD['MLR'] == True further enter method."""
+
+INPUT_METHOD_SSOM = {'SSOMnr': 'fCO2_Super_SOM_biome_1', 'nnnumber': 2}
+"""If INPUT_METHOD['SSOM'] == True further enter method."""
