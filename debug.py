4c622a5f703795ad3a30f7c18711c4d7518559ae
Søren <98187310+bergsoren@users.noreply.github.com>
2024-04-24 16:04:36 +0200

    maximum line length 79

--- debug.py
+++ debug.py
@@ -1,11 +1,13 @@
 """
-This file is for debugging and development functions, which will not be in the final project.
+This file is for debugging and development functions, which will not be in
+the final project.
 """
 
 debug = True
 
 def message(debugmessage):
-    """a version of print(), which can be turned off and on in a central location for debugging
+    """a version of print(), which can be turned off and on in a central
+    location for debugging
 
     Args:
         debugmessage (String): The debug message to be printed in the console.
