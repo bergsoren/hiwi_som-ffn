2b5bd4a2391093693ce605ec6a0e9bcb31b35bf7
Søren <98187310+bergsoren@users.noreply.github.com>
2024-04-24 16:05:36 +0200

    added first two input parameters which were original configured in GUI, maximum line length 79

--- settings.py
+++ settings.py
@@ -1,5 +1,15 @@
 """
-This file includes all settings and configurables
--> Everything that needs to be changed for the code to work will be found in this central location
+This file includes all settings and configurables.
+-> Everything that needs to be changed for the code to work will be found in
+this central location.
 """
+
+#========Input_Training_and_Labelling_new_GUI_v2021.m====
+INPUT_TIME = {'year_min': 1982, 'year_max': 2020, 'year2find': 2005,
+              'month2plot': 1}
+
+INPUT_GEOBORDERS = {'latmin': -90, 'latmax': 90, 'lonmin': -180,
+                    'lonmax': 180,'invareamin': 1,'invareamax': 11,
+                    'latplotmin': -90, 'latplotmax': 90, 'lonplotmin': -180,
+                    'lonplotmax': 180, 'version': 2};
 
